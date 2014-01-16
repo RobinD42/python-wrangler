4d67b405a80425822a4e35e9b988cfc696ae36c5
Robin Dunn <robin@alldunn.com>
2014-01-16 14:46:01 -0800

    Add py link Add bin link on Windows

--- activateEnv.sh
+++ activateEnv.sh
@@ -1,47 +1,9 @@
-#----------------------------------------------------------------------
-# Robin's Python Wrangler
-# 
-# This script can be used to easily switch the active Python (that
-# found first on the PATH) to any one of a set of Python environments
-# configured in ~/.myPyEnv and to keep track of which is active.  Both
-# full Python install trees and virtual environments can be managed in
-# this way.
 #
-# If you use cygwin bash on Windows with native Windows Pythons then
-# this script will also work for you there.  You just need to
-# configure your environments in ~/.myPyEnv with the cygwin paths,
-# not the Windows paths.  I expect that this script will also work with
-# cygwin Pythons too, but I don't use that myself so it hasn't been
-# tested.
 #
-# Once a Python has been activated then there will be some additional
-# command-line commands available to you:
+# NOTE: "source" this in bash, running normally in a subshell 
+#       will not have the desired effect.
 #
-#     deactivate:     Just like virtualenv this will reset your command-
-#                     line environment to what it was before the
-#                     activation.  The environments don't stack up, if
-#                     one is already active then it is deactivated
-#                     before activating the next one.
 #
-#     cdenv:          chdir to the root of the environment
-#
-#     cdsitepackages: chdir to the environment's site-packages folder.
-
-# NOTE: "source" this script in bash, running normally in a subshell
-#       will not have the desired effect.  An easy way to not have to
-#       remember this is to make a bash alias, such as:
-#
-#           alias activateEnv="source ~/bin/activateEnv.sh"
-#       or:
-#           alias workon="source ~/bin/activateEnv.sh"
-#
-#       if you have virtualenvwrapper muscle-memory
-#
-# NOTE2: This script assumes that there is already some python available 
-#        on the PATH that can be used to look up the environment paths
-#        that can be switched to.
-#
-#----------------------------------------------------------------------
 
 #set -o xtrace
 
@@ -101,35 +63,40 @@ main () {
 
     # Setup the Scripts dir on Windows.  It may not exist yet if
     # nothing has been installed, and we'll also add some aliases as
-    # symlinks so cygwin can find python.exe, etc.
+    # symlinks so cygwin can easily find python.exe, etc.
     if [ "$OSTYPE" == "cygwin" ]; then
-	if [ ! -e $BASEDIR/Scripts ]; then
-	    mkdir $BASEDIR/Scripts
+	if [ ! -e "$BASEDIR/Scripts" ]; then
+	    mkdir "$BASEDIR/Scripts"
 	fi
-	if [ ! -e $BASEDIR/Scripts/python.exe ]; then
-	    cd $BASEDIR/Scripts
+	if [ ! -e "$BASEDIR/Scripts/python.exe" ]; then
+	    cd "$BASEDIR/Scripts"
 	    ln -s ../python*.exe .
 	    cd -
 	fi
-	# Add a bin link too??  I like it, but others may not.
-	#if [ ! -e $BASEDIR/bin ]; then
-        #    ln -s Scripts $BASEDIR/bin
-	#fi
+	# add a bin link too
+	if [ ! -e "$BASEDIR/bin" ]; then
+            ln -s Scripts "$BASEDIR/bin"
+	fi
     fi
 
-    _ActivateScript=$BASEDIR/$BIN/activate
+    _ActivateScript="$BASEDIR/$BIN/activate"
 
     # Write the activation sctipt into the env if it 
     # doesn't already have one
-    if [ ! -e $_ActivateScript -o "$RESET" == "yes" ]; then
-        writeActivateScript $BASEDIR $1 $_ActivateScript $BIN
+    if [ ! -e "$_ActivateScript" -o "$RESET" == "yes" ]; then
+        writeActivateScript "$BASEDIR" $1 "$_ActivateScript" $BIN
     fi
 
-    if [ ! -e $BASEDIR/$BIN/python$EXT ]; then
+    if [ ! -e "$BASEDIR/$BIN/python$EXT" ]; then
 	echo "WARNING: No $BIN/python$EXE found in environment"
-	if [ -e $BASEDIR/$BIN/python3$EXT ]; then
+	if [ -e "$BASEDIR/$BIN/python3$EXT" ]; then
 	    echo -e "\tbut $BIN/python3$EXT does exist, be sure to use it instead."
 	fi
+    fi
+
+    # add my favorite alias
+    if [ ! -e "$BASEDIR/$BIN/py" ]; then
+	ln -s python $BASEDIR/$BIN/py
     fi
 
     return 0
@@ -144,12 +111,12 @@ writeActivateScript () {
     local SCRIPT=$3
     local BIN=$4
 
-    if [ -e $SCRIPT ]; then
-	mv $SCRIPT $SCRIPT.save
+    if [ -e "$SCRIPT" ]; then
+	mv "$SCRIPT" "$SCRIPT.save"
     fi
 
     echo "Writing new activate script."
-    cat <<-"EOF" | sed s!@BASEDIR@!$BASEDIR! | sed s!@PROMPT@!$PROMPT! | sed s!@BIN@!$BIN!  > $SCRIPT
+    cat <<-"EOF" | sed "s!@BASEDIR@!$BASEDIR!" | sed s!@PROMPT@!$PROMPT! | sed s!@BIN@!$BIN!  > "$SCRIPT"
 	# This file must be used with "source bin/activate" *from bash*
 	# you cannot run it directly
 	
@@ -262,12 +229,12 @@ fi
 
 cleanup
 
-if [ -z $_ActivateScript ]; then
+if [ -z "$_ActivateScript" ]; then
     return
 fi
 
 echo -e "Activating environment with:\n\t $_ActivateScript"
-source $_ActivateScript
+source "$_ActivateScript"
 unset _ActivateScript
 
 #set +o xtrace
